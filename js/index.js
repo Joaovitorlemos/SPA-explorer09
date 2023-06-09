ce3e3fa9dd40d9981d377e8f614ac57d870a3a87
João Vitor Lemos <75442354+Joaovitorlemos@users.noreply.github.com>
2023-06-09 01:29:25 -0300

    adding comments to study

--- js/index.js
+++ js/index.js
@@ -6,8 +6,10 @@ router.add("/about", "/pages/about.html")
 router.add("/contact", "/pages/contact.html")
 router.add(404, "/pages/404.html")
 
-
+// Para já iniciar o projeto na página Home
 router.handle()
 
+// Para poder navegar pelas rotas nas setinhas do navegador
 window.onpopstate = () => router.handle()
+
 window.route = () => router.route()
